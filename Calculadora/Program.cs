a672d316ec99ebd21a750139be8be3328bf38f8c
Javier <javier.jc942@gmail.com>
2026-05-05 06:55:43 -0400

    agregado de procedimientos

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -56,13 +56,13 @@
                 }
             } while (true);
         }
-        static string PedirNumero(string tipo, string caracteresValidos)
+        static string PedirNumero(string n, string tipo, string caracteresValidos)
         {
             string numero;
             bool valido;
             do
             {
-                Console.WriteLine($"Ingrese el número {tipo} (maximo 8bits):");
+                Console.Write($"Ingrese el {n} número {tipo} (maximo 8bits): ");
                 numero = Console.ReadLine()?.ToUpper() ?? "";
                 if (string.IsNullOrEmpty(numero))
                 {
@@ -83,126 +83,74 @@
             } while (!valido);
             return numero.PadLeft(8, '0');
         }
+        static void LlenarVector(string numero, int[] vector, int baseSistema)
+        {
+            int pos = 8 - numero.Length;
+            for (int i = 0; i < numero.Length; i++) vector[pos + i] = baseSistema == 16 ? Convert.ToInt32(numero[i].ToString(), 16) : numero[i] - '0';
+        }
+        static void Sumar(int baseSistema)
+        {
+            int acarreo = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                int suma = vector1[i] + vector2[i] + acarreo;
+                resultado[i] = suma % baseSistema;
+                acarreo = suma / baseSistema;
+            }
+            if (acarreo > 0)
+            {
+                Console.WriteLine($"acarreos: {acarreo}");
+                int[] temporal = new int[9];
+                temporal[0] = acarreo;
+                Array.Copy(resultado, 0, temporal, 1, 8);
+                resultado = temporal;
+            }
+        }
+        static void Restar(int baseSistema)
+        {
+            int prestamo = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                int resta = vector1[i] - vector2[i] - prestamo;
+                if (resta < 0)
+                {
+                    resultado[i] = resta + baseSistema;
+                    prestamo = 1;
+                }
+                else
+                {
+                    resultado[i] = resta;
+                    prestamo = 0;
+                }
+            }
+        }
         static void OperarVectores(char signo, int baseSistema)
         {
-            int acarreo = 0, prestamo = 0;
+            Array.Clear(vector1, 0, vector1.Length);
+            Array.Clear(vector2, 0, vector2.Length);
+            Array.Clear(resultado, 0, resultado.Length);
             string numero1, numero2;
             switch(baseSistema)
             {
                 case 2:
-                    numero1 = PedirNumero("Binario", "01");
-                    numero2 = PedirNumero("Binario", "01");
-                    for (int i = 0; i < numero1.Length; i++)
-                    {
-                        int pos = 8 - numero1.Length + i;
-                        vector1[i] = numero1[i] - '0';
-                    }
-                    for (int i = 0; i < numero2.Length; i++)
-                    {
-                        int pos = 8 - numero2.Length + i;
-                        vector2[i] = numero2[i] - '0';
-                    }
+                    numero1 = PedirNumero("primer", "Binario", "01");
+                    numero2 = PedirNumero("segundo", "Binario", "01");
                     break;
                 case 8:
-                    numero1 = PedirNumero("Octal", "01234567");
-                    numero2 = PedirNumero("Octal", "01234567");
-                    for (int i = 0; i < numero1.Length; i++)
-                    {
-                        int pos = 8 - numero1.Length + i;
-                        vector1[i] = numero1[i] - '0';
-                    }
-                    for (int i = 0; i < numero2.Length; i++)
-                    {
-                        int pos = 8 - numero2.Length + i;
-                        vector2[i] = numero2[i] - '0';
-                    }
+                    numero1 = PedirNumero("primer", "Octal", "01234567");
+                    numero2 = PedirNumero("segundo", "Octal", "01234567");
                     break;
                 case 16:
-                    numero1 = PedirNumero("Hexadecimal", "0123456789ABCDEF");
-                    numero2 = PedirNumero("Hexadecimal", "0123456789ABCDEF");
-                    for (int i = 0; i < numero1.Length; i++)
-                    {
-                        int pos = 8 - numero1.Length + i;
-                        switch (numero1[i])
-                        {
-                            case '0': vector1[pos] = 0; break;
-                            case '1': vector1[pos] = 1; break;
-                            case '2': vector1[pos] = 2; break;
-                            case '3': vector1[pos] = 3; break;
-                            case '4': vector1[pos] = 4; break;
-                            case '5': vector1[pos] = 5; break;
-                            case '6': vector1[pos] = 6; break;
-                            case '7': vector1[pos] = 7; break;
-                            case '8': vector1[pos] = 8; break;
-                            case '9': vector1[pos] = 9; break;
-                            case 'A': vector1[pos] = 10; break;
-                            case 'B': vector1[pos] = 11; break;
-                            case 'C': vector1[pos] = 12; break;
-                            case 'D': vector1[pos] = 13; break;
-                            case 'E': vector1[pos] = 14; break;
-                            case 'F': vector1[pos] = 15; break;
-                        }
-                    }
-                    for (int i = 0; i < numero2.Length; i++)
-                    {
-                        int pos = 8 - numero2.Length + i;
-                        switch (numero2[i])
-                        {
-                            case '0': vector2[pos] = 0; break;
-                            case '1': vector2[pos] = 1; break;
-                            case '2': vector2[pos] = 2; break;
-                            case '3': vector2[pos] = 3; break;
-                            case '4': vector2[pos] = 4; break;
-                            case '5': vector2[pos] = 5; break;
-                            case '6': vector2[pos] = 6; break;
-                            case '7': vector2[pos] = 7; break;
-                            case '8': vector2[pos] = 8; break;
-                            case '9': vector2[pos] = 9; break;
-                            case 'A': vector2[pos] = 10; break;
-                            case 'B': vector2[pos] = 11; break;
-                            case 'C': vector2[pos] = 12; break;
-                            case 'D': vector2[pos] = 13; break;
-                            case 'E': vector2[pos] = 14; break;
-                            case 'F': vector2[pos] = 15; break;
-                        }
-                    }
+                    numero1 = PedirNumero("primer", "Hexadecimal", "0123456789ABCDEF");
+                    numero2 = PedirNumero("segundo", "Hexadecimal", "0123456789ABCDEF");
                     break;
+                default:return;
             }
-            if (signo == '+')
-            {
-                for (int i = 7; i >= 0; i--)
-                {
-                    int suma = vector1[i] + vector2[i] + acarreo;
-                    resultado[i] = suma % baseSistema;
-                    acarreo = suma / baseSistema;
-                }
-                if (acarreo > 0)
-                {
-                    int[] vectorExpandido = new int[9];
-                    vectorExpandido[0] = acarreo;
-                    Array.Copy(resultado, 0, vectorExpandido, 1, 8);
-                    resultado = vectorExpandido;
-                }
-            }
-            else // signo == '-'
-            {
-                for (int i = 7; i >= 0; i--)
-                {
-                    int resta = vector1[i] - vector2[i] - prestamo;
-                    if (resta < 0)
-                    {
-                        resultado[i] = resta + baseSistema;
-                        prestamo = 1;
-                    }
-                    else
-                    {
-                        resultado[i] = resta;
-                        prestamo = 0;
-                    }
-                }
-            }
-            Console.WriteLine($"Vector 1 cargado: {string.Join("", vector1).TrimStart('0')} | Vector 2 cargado: {string.Join("", vector2).TrimStart('0')}");
-            Console.WriteLine($"Resultado: {string.Join("", resultado.Select(d => d.ToString("X"))).TrimStart('0')}");
+            LlenarVector(numero1, vector1, baseSistema);
+            LlenarVector(numero2, vector2, baseSistema);
+            if (signo == '+') Sumar(baseSistema);
+            else Restar(baseSistema);
+            Console.WriteLine($"Vector 1 cargado: {(string.Join("", vector1).TrimStart('0') == "" ? "0" : string.Join("", vector1).TrimStart('0'))}\nVector 2 cargado: {(string.Join("", vector2).TrimStart('0') == "" ? "0" : string.Join("", vector2).TrimStart('0'))}\nResultado: {(string.Join("", resultado.Select(d => d.ToString("X"))).TrimStart('0') == "" ? "0" : string.Join("", resultado.Select(d => d.ToString("X"))).TrimStart('0'))}");
             Console.ReadKey();
         }
     }
